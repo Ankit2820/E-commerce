891d78ac7c82f4b0f7c494a29f967e75289ee378
Ankit2820 <102996168+Ankit2820@users.noreply.github.com>
2023-03-28 12:41:06 +0530

    Update Product.jsx

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -4,15 +4,18 @@ import Announcement from "../components/Announcement"
 import Footer from "../components/Footer"
 import Navbar from "../components/Navbar"
 import Newsletter from "../components/Newsletter"
-import { mobile } from "../responsive";
 const Container = styled.div`
 
 `
 const Wrapper = styled.div`
    padding: 50px;
    display: flex;
-   ${mobile({ padding: "10px", flexDirection:"column" })}
+   @media only screen and (max-width: 390px){
+   padding: 10px;
+   flex-direction: column;
+   }
 `
+
 const ImgContainer = styled.div`
    flex:1;
 `
@@ -20,13 +23,19 @@ const Image = styled.img`
     width: 100%;
     height: 90vh;
     object-fit: cover;
-    ${mobile({ height: "40vh" })}
-`
+    @media only screen and (max-width: 390px){
+      height: 40vh;
+      }
+   `
+  
 const InfoContainer = styled.div`
     flex:1;
     padding: 0px 50px;
-    ${mobile({ padding: "10px" })}
-`
+    @media only screen and (max-width: 390px){
+      padding: 10px;
+      }
+   `
+
 const Title = styled.h1`
    font-weight: 200;
 `
@@ -42,8 +51,7 @@ const FilterContainer = styled.div`
      margin: 30px 0px;
      display: flex;
      justify-content: space-between;
-     ${mobile({ width: "100%" })}
-`
+
 const Filter = styled.div`
     display: flex;
     align-items: center;
@@ -52,6 +60,7 @@ const FilterTitle = styled.span`
       font-size: 20px;
       font-weight: 200;
 `
+     
 const FilterColor = styled.div`
      width: 20px;
      height: 20px;
@@ -72,8 +81,10 @@ const AddContainer = styled.div`
     display: flex;
     align-items: center;
     justify-content: space-between;
-    ${mobile({ width: "100%" })}
-`
+    @media only screen and (max-width: 390px){
+       width: 100%;
+      }
+   `
 const AmountContainer = styled.div`
      display: flex;
      align-items: center;
